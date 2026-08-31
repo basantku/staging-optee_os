qualcomm-linux/optee_os

pta: qcom: pas: add firmware segment hash verification
A compromised REE could substitute firmware in the peripheral's
carveout between the point image metadata is accepted and the point
the peripheral is released from reset. Add a verification step that
re-hashes each loaded segment against the image's own digest table
and fails closed on any mismatch, so the peripheral only runs code
whose bytes match what was authenticated.

Provide it as a standalone capability so hash verification can be
enabled and reviewed independently of signature authentication.

Signed-off-by: Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
Assisted-by: Claude:sonnet-5
Reviewed-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

core/pta/qcom/pas/sub.mk
@@ -1,5 +1,6 @@
 srcs-y += pta_qcom_pas.c
 srcs-y += pas_core.c
+srcs-$(CFG_QCOM_PAS_AUTH) += pas_auth_core.c
 incdirs-y += .
 incdirs-y += platform/
 subdirs-y += platform/$(PLATFORM_FLAVOR)

basantku/staging-optee_os

Acked-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

core/pta/qcom/pas/sub.mk
@@ -1,5 +1,6 @@
 srcs-y += pta_qcom_pas.c
 srcs-y += pas_core.c
+srcs-$(CFG_QCOM_PAS_AUTH) += pas_auth.c
 incdirs-y += .
 incdirs-y += platform/
 subdirs-y += platform/$(PLATFORM_FLAVOR)