qualcomm-linux/optee_os

ta: qcom_pas: verify loaded firmware image integrity
Segment-hash verification lives in the PTA but nothing invokes it
today, so a peripheral can be released from reset without any
TEE-side check that the firmware in its carveout is what was
authenticated. Bridge that gap: the TA captures a TEE-private copy
of the REE-supplied metadata at image-init time and drives the
verifier with it at reset time, so the REE cannot mutate the
metadata the check runs against between the two events.

Per-peripheral state is keyed by ID so concurrent bring-ups on the
same session do not clobber each other. Signature authentication is
left as a placeholder so segment-hash verification can land and be
reviewed without waiting on it.

Release the captured metadata when a peripheral is shut down, so it
does not remain allocated for the life of the session after the
peripheral it authenticated is torn down. The per-ID slot itself is
retained because slots are provisioned one per peripheral;
reclaiming it would break the capacity model when the same
peripheral is reloaded later.

Signed-off-by: Selvam Sathappan Periakaruppan <speriaka@qti.qualcomm.com>
Assisted-by: Claude:sonnet-5
Reviewed-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>

ta/qcom_pas/src/pas_auth.c
@@ -1,0 +1,178 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include <pas_auth.h>
+#include <pas_mbn_parser.h>
+#include <pas_meta.h>
+#include <pta_qcom_pas.h>
+#include <qcom_pas_priv.h>
+#include <string.h>
+#include <tee_internal_api.h>
+#include <utee_defines.h>
+
+static struct pas_md_slot *get_meta_data_slot(struct qcom_pas_session *s,
+					      uint32_t pas_id)
+{
+	size_t i = 0;
+
+	for (i = 0; i < PAS_MD_SLOTS; i++) {
+		if (s->slots[i].in_use && s->slots[i].pas_id == pas_id)
+			return &s->slots[i];
+	}
+
+	return NULL;
+}
+
+TEE_Result pas_auth_save_metadata(struct qcom_pas_session *s, uint32_t pt,
+				  TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	struct pas_md_slot *slot = NULL;
+	void *meta_data_copy = NULL;
+	uint32_t pas_id = 0;
+	size_t size = 0;
+	size_t i = 0;
+
+	if (pt != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	size = params[1].memref.size;
+	if (size && !params[1].memref.buffer)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	pas_id = params[0].value.a;
+
+	slot = get_meta_data_slot(s, pas_id);
+	if (!slot) {
+		for (i = 0; i < PAS_MD_SLOTS; i++) {
+			if (!s->slots[i].in_use) {
+				slot = &s->slots[i];
+				goto found;
+			}
+		}
+		EMSG("PAS auth: no free md slot (pas_id=%#"PRIx32")", pas_id);
+		return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+found:
+	if (size) {
+		meta_data_copy = TEE_Malloc(size, TEE_MALLOC_FILL_ZERO);
+		if (!meta_data_copy)
+			return TEE_ERROR_OUT_OF_MEMORY;
+		memcpy(meta_data_copy, params[1].memref.buffer, size);
+	}
+
+	TEE_Free(slot->meta_data);
+	slot->meta_data = meta_data_copy;
+	slot->meta_data_size = size;
+	slot->pas_id = pas_id;
+	slot->in_use = true;
+	slot->ready = false;
+	memset(&slot->mbn, 0, sizeof(slot->mbn));
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result pas_auth_authenticate(struct qcom_pas_session *s, uint32_t pas_id)
+{
+	struct pas_md_slot *slot = get_meta_data_slot(s, pas_id);
+	TEE_Result res = TEE_ERROR_GENERIC;
+	uint32_t hash_len = 0;
+
+	if (!slot) {
+		EMSG("PAS auth: no metadata for pas_id=%#"PRIx32
+		     " (call INIT_IMAGE first)", pas_id);
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	res = pas_meta_segment_hash_len(slot->meta_data, slot->meta_data_size,
+					&hash_len);
+	if (res) {
+		EMSG("PAS auth: cannot pick hash size: %#"PRIx32, res);
+		return res;
+	}
+
+	res = pas_mbn_parse(slot->meta_data, slot->meta_data_size, hash_len,
+			    &slot->mbn);
+	if (res) {
+		EMSG("PAS auth: MBN parse failed: %#"PRIx32, res);
+		return res;
+	}
+
+	slot->ready = true;
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result pas_auth_verify_reset(struct qcom_pas_session *s,
+				 TEE_TASessionHandle pta_session,
+				 uint32_t pas_id,
+				 TEE_Param params[TEE_NUM_PARAMS])
+{
+	struct pas_md_slot *slot = get_meta_data_slot(s, pas_id);
+	TEE_Param vp[TEE_NUM_PARAMS] = { };
+	TEE_Result res = TEE_ERROR_GENERIC;
+	size_t buffer_size = 0;
+	uint8_t *buffer = NULL;
+	uint32_t pt = 0;
+
+	if (!slot || !slot->ready) {
+		EMSG("PAS auth: pas_id=%#"PRIx32
+		     " is not ready (call INIT_IMAGE first)", pas_id);
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	if (ADD_OVERFLOW(slot->meta_data_size, slot->mbn.hash_table_size,
+			 &buffer_size))
+		return TEE_ERROR_OVERFLOW;
+
+	buffer = TEE_Malloc(buffer_size, TEE_MALLOC_FILL_ZERO);
+	if (!buffer)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	TEE_MemMove(buffer, slot->meta_data, slot->meta_data_size);
+	TEE_MemMove(buffer + slot->meta_data_size, slot->mbn.hash_table,
+		    slot->mbn.hash_table_size);
+
+	pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+			     TEE_PARAM_TYPE_VALUE_INPUT,
+			     TEE_PARAM_TYPE_MEMREF_INPUT,
+			     TEE_PARAM_TYPE_VALUE_INPUT);
+
+	vp[0].value.a = params[0].value.a;
+	vp[0].value.b = params[0].value.b;
+	vp[1].value.a = params[1].value.a;
+	vp[1].value.b = params[1].value.b;
+	vp[2].memref.buffer = buffer;
+	vp[2].memref.size = buffer_size;
+	vp[3].value.a = slot->mbn.hash_len;
+	vp[3].value.b = slot->meta_data_size;
+
+	res = TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
+				  PTA_QCOM_PAS_VERIFY_IMAGE, pt, vp, NULL);
+
+	TEE_Free(buffer);
+	return res;
+}
+
+TEE_Result pas_auth_release_metadata(struct qcom_pas_session *s,
+				     uint32_t pas_id)
+{
+	struct pas_md_slot *slot = get_meta_data_slot(s, pas_id);
+
+	if (!slot)
+		return TEE_SUCCESS;
+
+	TEE_Free(slot->meta_data);
+	slot->meta_data = NULL;
+	slot->meta_data_size = 0;
+	slot->ready = false;
+	memset(&slot->mbn, 0, sizeof(slot->mbn));
+
+	return TEE_SUCCESS;
+}

ta/qcom_pas/src/qcom_pas.c
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
+#include <pas_auth.h>
 #include <pta_qcom_pas.h>
+#include <qcom_pas_priv.h>
 #include <ta_qcom_pas.h>
 #include <tee_internal_api.h>
 #include <tee_internal_api_extensions.h>
@@ -13,21 +15,63 @@
 static size_t session_refcount;
 static TEE_TASessionHandle pta_session;
 
-static TEE_Result qcom_pas_auth_and_reset(uint32_t pt,
+static TEE_Result qcom_pas_init_image(struct qcom_pas_session *s, uint32_t pt,
+				      TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	res = TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
+				  PTA_QCOM_PAS_INIT_IMAGE, pt, params, NULL);
+	if (res)
+		return res;
+
+	res = pas_auth_save_metadata(s, pt, params);
+	if (res)
+		return res;
+
+	return pas_auth_authenticate(s, params[0].value.a);
+}
+
+static TEE_Result qcom_pas_auth_and_reset(struct qcom_pas_session *s,
+					  uint32_t pt,
 					  TEE_Param params[TEE_NUM_PARAMS])
 {
 	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
 						TEE_PARAM_TYPE_VALUE_INPUT,
 						TEE_PARAM_TYPE_MEMREF_INPUT,
 						TEE_PARAM_TYPE_NONE);
+	TEE_Result res = TEE_ERROR_GENERIC;
+
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	/* Firmware authentication - TODO */
+	res = pas_auth_verify_reset(s, pta_session, params[0].value.a, params);
+	if (res)
+		return res;
 
 	return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
 				   PTA_QCOM_PAS_AUTH_AND_RESET,
 				   pt, params, NULL);
+}
+
+static TEE_Result qcom_pas_shutdown(struct qcom_pas_session *s, uint32_t pt,
+				    TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	if (pt != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	res = TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
+				  PTA_QCOM_PAS_SHUTDOWN, pt, params, NULL);
+
+	pas_auth_release_metadata(s, params[0].value.a);
+
+	return res;
 }
 
 TEE_Result TA_CreateEntryPoint(void)
@@ -41,56 +85,78 @@ void TA_DestroyEntryPoint(void)
 
 TEE_Result TA_OpenSessionEntryPoint(uint32_t pt,
 				    TEE_Param params[TEE_NUM_PARAMS],
-				    void **sess __unused)
+				    void **sess_ctx)
 {
 	static const TEE_UUID uuid = PTA_QCOM_PAS_UUID;
-	TEE_Result res = TEE_ERROR_GENERIC;
 	TEE_PropSetHandle h = TEE_HANDLE_NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct qcom_pas_session *s = NULL;
 	TEE_Identity id = { };
 
 	res = TEE_AllocatePropertyEnumerator(&h);
-	if (res != TEE_SUCCESS)
-		goto error;
+	if (res)
+		goto out;
 
 	TEE_StartPropertyEnumerator(h, TEE_PROPSET_CURRENT_CLIENT);
 
 	res = TEE_GetPropertyAsIdentity(h, NULL, &id);
-	if (res != TEE_SUCCESS)
-		goto error;
+	if (res)
+		goto out;
 
 	if (id.login != TEE_LOGIN_REE_KERNEL) {
 		res = TEE_ERROR_ACCESS_DENIED;
-		goto error;
+		goto out;
+	}
+
+	s = TEE_Malloc(sizeof(*s), TEE_MALLOC_FILL_ZERO);
+	if (!s) {
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
 	}
 
 	if (!session_refcount) {
 		res = TEE_OpenTASession(&uuid, TEE_TIMEOUT_INFINITE, pt, params,
 					&pta_session, NULL);
-		if (res != TEE_SUCCESS)
-			goto error;
+		if (res) {
+			TEE_Free(s);
+			goto out;
+		}
 	}
 
 	session_refcount++;
-	res = TEE_SUCCESS;
-error:
+	*sess_ctx = s;
+
+out:
 	if (h)
 		TEE_FreePropertyEnumerator(h);
 
 	return res;
 }
 
-void TA_CloseSessionEntryPoint(void *sess __unused)
+void TA_CloseSessionEntryPoint(void *sess_ctx)
 {
+	struct qcom_pas_session *s = sess_ctx;
+
+	if (s) {
+		size_t i = 0;
+
+		for (i = 0; i < PAS_MD_SLOTS; i++)
+			TEE_Free(s->slots[i].meta_data);
+		TEE_Free(s);
+	}
+
 	session_refcount--;
 
 	if (!session_refcount)
 		TEE_CloseTASession(pta_session);
 }
 
-TEE_Result TA_InvokeCommandEntryPoint(void *sess __unused, uint32_t cmd_id,
+TEE_Result TA_InvokeCommandEntryPoint(void *sess_ctx, uint32_t cmd_id,
 				      uint32_t pt,
 				      TEE_Param params[TEE_NUM_PARAMS])
 {
+	struct qcom_pas_session *s = sess_ctx;
+
 	switch (cmd_id) {
 	case TA_QCOM_PAS_IS_SUPPORTED:
 		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
@@ -101,9 +167,7 @@ TEE_Result TA_InvokeCommandEntryPoint(void *sess __unused, uint32_t cmd_id,
 					   PTA_QCOM_PAS_CAPABILITIES,
 					   pt, params, NULL);
 	case TA_QCOM_PAS_INIT_IMAGE:
-		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
-					   PTA_QCOM_PAS_INIT_IMAGE,
-					   pt, params, NULL);
+		return qcom_pas_init_image(s, pt, params);
 	case TA_QCOM_PAS_MEM_SETUP:
 		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
 					   PTA_QCOM_PAS_MEM_SETUP,
@@ -113,15 +177,13 @@ TEE_Result TA_InvokeCommandEntryPoint(void *sess __unused, uint32_t cmd_id,
 					   PTA_QCOM_PAS_GET_RESOURCE_TABLE,
 					   pt, params, NULL);
 	case TA_QCOM_PAS_AUTH_AND_RESET:
-		return qcom_pas_auth_and_reset(pt, params);
+		return qcom_pas_auth_and_reset(s, pt, params);
 	case TA_QCOM_PAS_SET_REMOTE_STATE:
 		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
 					   PTA_QCOM_PAS_SET_REMOTE_STATE,
 					   pt, params, NULL);
 	case TA_QCOM_PAS_SHUTDOWN:
-		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
-					   PTA_QCOM_PAS_SHUTDOWN,
-					   pt, params, NULL);
+		return qcom_pas_shutdown(s, pt, params);
 	default:
 		return TEE_ERROR_NOT_IMPLEMENTED;
 	}

basantku/staging-optee_os

Acked-by: Jens Wiklander <jens.wiklander@oss.qualcomm.com>

ta/qcom_pas/src/auth/pas_auth.c
@@ -1,0 +1,177 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#include <auth/pas_auth.h>
+#include <auth/pas_mbn.h>
+#include <auth/pas_meta.h>
+#include <pta_qcom_pas.h>
+#include <string.h>
+#include <tee_internal_api.h>
+#include <utee_defines.h>
+
+static struct pas_md_slot *get_meta_data_slot(struct qcom_pas_session *s,
+					      uint32_t pas_id)
+{
+	size_t i = 0;
+
+	for (i = 0; i < PAS_MD_SLOTS; i++) {
+		if (s->slots[i].in_use && s->slots[i].pas_id == pas_id)
+			return &s->slots[i];
+	}
+
+	return NULL;
+}
+
+TEE_Result pas_auth_save_metadata(struct qcom_pas_session *s, uint32_t pt,
+				  TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	struct pas_md_slot *slot = NULL;
+	void *meta_data_copy = NULL;
+	uint32_t pas_id = 0;
+	size_t size = 0;
+	size_t i = 0;
+
+	if (pt != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	size = params[1].memref.size;
+	if (size && !params[1].memref.buffer)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	pas_id = params[0].value.a;
+
+	slot = get_meta_data_slot(s, pas_id);
+	if (!slot) {
+		for (i = 0; i < PAS_MD_SLOTS; i++) {
+			if (!s->slots[i].in_use) {
+				slot = &s->slots[i];
+				goto found;
+			}
+		}
+		EMSG("PAS auth: no free md slot (pas_id=%#"PRIx32")", pas_id);
+		return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+found:
+	if (size) {
+		meta_data_copy = TEE_Malloc(size, TEE_MALLOC_FILL_ZERO);
+		if (!meta_data_copy)
+			return TEE_ERROR_OUT_OF_MEMORY;
+		memcpy(meta_data_copy, params[1].memref.buffer, size);
+	}
+
+	TEE_Free(slot->meta_data);
+	slot->meta_data = meta_data_copy;
+	slot->meta_data_size = size;
+	slot->pas_id = pas_id;
+	slot->in_use = true;
+	slot->ready = false;
+	memset(&slot->mbn, 0, sizeof(slot->mbn));
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result pas_auth_prepare(struct qcom_pas_session *s, uint32_t pas_id)
+{
+	struct pas_md_slot *slot = get_meta_data_slot(s, pas_id);
+	TEE_Result res = TEE_ERROR_GENERIC;
+	uint32_t hash_len = 0;
+
+	if (!slot) {
+		EMSG("PAS auth: no metadata for pas_id=%#"PRIx32
+		     " (call INIT_IMAGE first)", pas_id);
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	res = pas_meta_segment_hash_len(slot->meta_data, slot->meta_data_size,
+					&hash_len);
+	if (res) {
+		EMSG("PAS auth: cannot pick hash size: %#"PRIx32, res);
+		return res;
+	}
+
+	res = pas_mbn_parse(slot->meta_data, slot->meta_data_size, hash_len,
+			    &slot->mbn);
+	if (res) {
+		EMSG("PAS auth: MBN parse failed: %#"PRIx32, res);
+		return res;
+	}
+
+	slot->ready = true;
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result pas_auth_verify(struct qcom_pas_session *s,
+			   TEE_TASessionHandle pta_session,
+			   uint32_t pas_id,
+			   TEE_Param params[TEE_NUM_PARAMS])
+{
+	struct pas_md_slot *slot = get_meta_data_slot(s, pas_id);
+	TEE_Param vp[TEE_NUM_PARAMS] = { };
+	TEE_Result res = TEE_ERROR_GENERIC;
+	size_t buffer_size = 0;
+	uint8_t *buffer = NULL;
+	uint32_t pt = 0;
+
+	if (!slot || !slot->ready) {
+		EMSG("PAS auth: pas_id=%#"PRIx32
+		     " is not ready (call INIT_IMAGE first)", pas_id);
+		return TEE_ERROR_BAD_STATE;
+	}
+
+	if (ADD_OVERFLOW(slot->meta_data_size, slot->mbn.hash_table_size,
+			 &buffer_size))
+		return TEE_ERROR_OVERFLOW;
+
+	buffer = TEE_Malloc(buffer_size, TEE_MALLOC_FILL_ZERO);
+	if (!buffer)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	TEE_MemMove(buffer, slot->meta_data, slot->meta_data_size);
+	TEE_MemMove(buffer + slot->meta_data_size, slot->mbn.hash_table,
+		    slot->mbn.hash_table_size);
+
+	pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+			     TEE_PARAM_TYPE_VALUE_INPUT,
+			     TEE_PARAM_TYPE_MEMREF_INPUT,
+			     TEE_PARAM_TYPE_VALUE_INPUT);
+
+	vp[0].value.a = params[0].value.a;
+	vp[0].value.b = params[0].value.b;
+	vp[1].value.a = params[1].value.a;
+	vp[1].value.b = params[1].value.b;
+	vp[2].memref.buffer = buffer;
+	vp[2].memref.size = buffer_size;
+	vp[3].value.a = slot->mbn.hash_len;
+	vp[3].value.b = slot->meta_data_size;
+
+	res = TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
+				  PTA_QCOM_PAS_VERIFY_IMAGE, pt, vp, NULL);
+
+	TEE_Free(buffer);
+	return res;
+}
+
+TEE_Result pas_auth_release_metadata(struct qcom_pas_session *s,
+				     uint32_t pas_id)
+{
+	struct pas_md_slot *slot = get_meta_data_slot(s, pas_id);
+
+	if (!slot)
+		return TEE_SUCCESS;
+
+	TEE_Free(slot->meta_data);
+	slot->meta_data = NULL;
+	slot->meta_data_size = 0;
+	slot->ready = false;
+	memset(&slot->mbn, 0, sizeof(slot->mbn));
+
+	return TEE_SUCCESS;
+}

ta/qcom_pas/src/qcom_pas.c
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
+#include <auth/pas_auth.h>
+#include <auth/pas_meta.h>
 #include <pta_qcom_pas.h>
 #include <ta_qcom_pas.h>
 #include <tee_internal_api.h>
@@ -13,21 +15,63 @@
 static size_t session_refcount;
 static TEE_TASessionHandle pta_session;
 
-static TEE_Result qcom_pas_auth_and_reset(uint32_t pt,
+static TEE_Result qcom_pas_init_image(struct qcom_pas_session *s, uint32_t pt,
+				      TEE_Param params[TEE_NUM_PARAMS])
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	res = TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
+				  PTA_QCOM_PAS_INIT_IMAGE, pt, params, NULL);
+	if (res)
+		return res;
+
+	res = pas_auth_save_metadata(s, pt, params);
+	if (res)
+		return res;
+
+	return pas_auth_prepare(s, params[0].value.a);
+}
+
+static TEE_Result qcom_pas_auth_and_reset(struct qcom_pas_session *s,
+					  uint32_t pt,
 					  TEE_Param params[TEE_NUM_PARAMS])
 {
 	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
 						TEE_PARAM_TYPE_VALUE_INPUT,
 						TEE_PARAM_TYPE_MEMREF_INPUT,
 						TEE_PARAM_TYPE_NONE);
+	TEE_Result res = TEE_ERROR_GENERIC;
+
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
 
-	/* Firmware authentication - TODO */
+	res = pas_auth_verify(s, pta_session, params[0].value.a, params);
+	if (res)
+		return res;
 
 	return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
 				   PTA_QCOM_PAS_AUTH_AND_RESET,
 				   pt, params, NULL);
+}
+
+static TEE_Result qcom_pas_shutdown(struct qcom_pas_session *s, uint32_t pt,
+				    TEE_Param params[TEE_NUM_PARAMS])
+{
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE,
+						TEE_PARAM_TYPE_NONE);
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	if (pt != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	res = TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
+				  PTA_QCOM_PAS_SHUTDOWN, pt, params, NULL);
+
+	pas_auth_release_metadata(s, params[0].value.a);
+
+	return res;
 }
 
 TEE_Result TA_CreateEntryPoint(void)
@@ -41,56 +85,78 @@ void TA_DestroyEntryPoint(void)
 
 TEE_Result TA_OpenSessionEntryPoint(uint32_t pt,
 				    TEE_Param params[TEE_NUM_PARAMS],
-				    void **sess __unused)
+				    void **sess_ctx)
 {
 	static const TEE_UUID uuid = PTA_QCOM_PAS_UUID;
-	TEE_Result res = TEE_ERROR_GENERIC;
 	TEE_PropSetHandle h = TEE_HANDLE_NULL;
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct qcom_pas_session *s = NULL;
 	TEE_Identity id = { };
 
 	res = TEE_AllocatePropertyEnumerator(&h);
-	if (res != TEE_SUCCESS)
-		goto error;
+	if (res)
+		goto out;
 
 	TEE_StartPropertyEnumerator(h, TEE_PROPSET_CURRENT_CLIENT);
 
 	res = TEE_GetPropertyAsIdentity(h, NULL, &id);
-	if (res != TEE_SUCCESS)
-		goto error;
+	if (res)
+		goto out;
 
 	if (id.login != TEE_LOGIN_REE_KERNEL) {
 		res = TEE_ERROR_ACCESS_DENIED;
-		goto error;
+		goto out;
+	}
+
+	s = TEE_Malloc(sizeof(*s), TEE_MALLOC_FILL_ZERO);
+	if (!s) {
+		res = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
 	}
 
 	if (!session_refcount) {
 		res = TEE_OpenTASession(&uuid, TEE_TIMEOUT_INFINITE, pt, params,
 					&pta_session, NULL);
-		if (res != TEE_SUCCESS)
-			goto error;
+		if (res) {
+			TEE_Free(s);
+			goto out;
+		}
 	}
 
 	session_refcount++;
-	res = TEE_SUCCESS;
-error:
+	*sess_ctx = s;
+
+out:
 	if (h)
 		TEE_FreePropertyEnumerator(h);
 
 	return res;
 }
 
-void TA_CloseSessionEntryPoint(void *sess __unused)
+void TA_CloseSessionEntryPoint(void *sess_ctx)
 {
+	struct qcom_pas_session *s = sess_ctx;
+
+	if (s) {
+		size_t i = 0;
+
+		for (i = 0; i < PAS_MD_SLOTS; i++)
+			TEE_Free(s->slots[i].meta_data);
+		TEE_Free(s);
+	}
+
 	session_refcount--;
 
 	if (!session_refcount)
 		TEE_CloseTASession(pta_session);
 }
 
-TEE_Result TA_InvokeCommandEntryPoint(void *sess __unused, uint32_t cmd_id,
+TEE_Result TA_InvokeCommandEntryPoint(void *sess_ctx, uint32_t cmd_id,
 				      uint32_t pt,
 				      TEE_Param params[TEE_NUM_PARAMS])
 {
+	struct qcom_pas_session *s = sess_ctx;
+
 	switch (cmd_id) {
 	case TA_QCOM_PAS_IS_SUPPORTED:
 		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
@@ -101,9 +167,7 @@ TEE_Result TA_InvokeCommandEntryPoint(void *sess __unused, uint32_t cmd_id,
 					   PTA_QCOM_PAS_CAPABILITIES,
 					   pt, params, NULL);
 	case TA_QCOM_PAS_INIT_IMAGE:
-		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
-					   PTA_QCOM_PAS_INIT_IMAGE,
-					   pt, params, NULL);
+		return qcom_pas_init_image(s, pt, params);
 	case TA_QCOM_PAS_MEM_SETUP:
 		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
 					   PTA_QCOM_PAS_MEM_SETUP,
@@ -113,15 +177,13 @@ TEE_Result TA_InvokeCommandEntryPoint(void *sess __unused, uint32_t cmd_id,
 					   PTA_QCOM_PAS_GET_RESOURCE_TABLE,
 					   pt, params, NULL);
 	case TA_QCOM_PAS_AUTH_AND_RESET:
-		return qcom_pas_auth_and_reset(pt, params);
+		return qcom_pas_auth_and_reset(s, pt, params);
 	case TA_QCOM_PAS_SET_REMOTE_STATE:
 		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
 					   PTA_QCOM_PAS_SET_REMOTE_STATE,
 					   pt, params, NULL);
 	case TA_QCOM_PAS_SHUTDOWN:
-		return TEE_InvokeTACommand(pta_session, TEE_TIMEOUT_INFINITE,
-					   PTA_QCOM_PAS_SHUTDOWN,
-					   pt, params, NULL);
+		return qcom_pas_shutdown(s, pt, params);
 	default:
 		return TEE_ERROR_NOT_IMPLEMENTED;
 	}